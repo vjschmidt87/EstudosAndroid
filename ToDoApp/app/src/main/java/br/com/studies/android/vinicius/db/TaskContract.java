31dfcb80784ff901bbf5434a33894916cec98d65
Vinicius Jose Schmidt <vinicius.schmidt@cwi.com.br>
2017-03-29 17:02:16 -0300

    inicio ToDoApp

---
+++ ToDoApp/app/src/main/java/br/com/studies/android/vinicius/db/TaskContract.java
@@ -0,0 +1,10 @@
+package br.com.studies.android.vinicius.db;
+
+/**
+ * Created by vinicius.schmidt on 29/03/2017.
+ */
+
+public class TaskContract {
+    public static final String DB_NAME = "br.com.studies.android.vinicius.db";
+    public static final int DB_VERSION = 1;
+}
---
+++ ToDoApp/app/src/main/java/br/com/studies/android/vinicius/db/TaskEntry.java
@@ -0,0 +1,13 @@
+package br.com.studies.android.vinicius.db;
+
+import android.provider.BaseColumns;
+
+/**
+ * Created by vinicius.schmidt on 29/03/2017.
+ */
+
+public class TaskEntry implements BaseColumns {
+    public static final String TABLE = "tasks";
+
+    public static final String COL_TASK_TITLE = "title";
+}
